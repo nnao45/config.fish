31144dbec25b2f3bdcec4629bb48a3cbdad815a2
nnao45 <n4sekai5y@gmail.com>
2018-01-31 17:02:37 +0900

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,11 @@
+set fish_theme shellder
+set fish_greeting
+
+set -x LANG en_US.utf8
+set -x GOBIN /Users/nnao45/go/bin/
+set -x GOROOT /usr/src/go
+set -x GOPATH /usr/src/go-third-party
+set PATH $GOPATH/bin $GOROOT/bin $PATH
+
+alias rm='rm -i'
+alias cp='cp -i'
